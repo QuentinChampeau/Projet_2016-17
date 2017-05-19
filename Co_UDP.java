d24bcf02da16f6c4cf72902455f95f40d56242df
Hearner <quentin_ch@hotmail.fr>
2017-05-19 04:16:09 +0200

    Update Co_UDP.java

--- Co_UDP.java
+++ Co_UDP.java
@@ -10,99 +10,105 @@ import java.nio.ByteBuffer;
 
 /**
  *
- * @author Jérémi
+ * @author Jérémi Quentin
  */
 public class Co_UDP {
-    public final static int port = 2345;
-    //Avion[] avion;
-    ByteBuffer B;
-    int nbAvion,i,n;
-    String[] S;
-   
-    public void Connexion(){
-    System.out.println("Connexion...");
-                try {
-                    //Création de la connexion côté serveur, en spécifiant un port d'écoute
-                    //DatagramSocket server = new DatagramSocket(port);
+	public final static int port = 2345;
+    	//Avion[] avion;
+	ByteBuffer B;
+	int nbAvion,i,n;
+	String[] S;
+
+	/**
+	 *
+	 * @param
+	 * @return 
+	 */
+	public void Connexion() {
+		System.out.println("Connexion...");
+		try {
+            // Création de la connexion côté serveur, en spécifiant un port d'écoute
+            // DatagramSocket server = new DatagramSocket(port);
 			InetAddress group = InetAddress.getByName("224.0.0.1");
 			MulticastSocket s = new MulticastSocket(port);
 			s.joinGroup(group);
-			
 
-                    while(true){
 
-                        //On s'occupe maintenant de l'objet paquet
-                        byte[] buffer = new byte[8000];
-                        //byte rawCode[] = new byte[5];
-                        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			while(true) {
 
-                        //Cette méthode permet de récupérer le datagramme envoyé par le client
-                        //Elle bloque le thread jusqu'à ce que celui-ci ait reçu quelque chose.
-                        s.receive(packet);
+                // On s'occupe maintenant de l'objet paquet
+				byte[] buffer = new byte[8000];
+                // byte rawCode[] = new byte[5];
+				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
-			String chaine = new String(packet.getData());
-                        /*B = ByteBuffer.wrap(packet.getData());
-                        nbAvion=B.getInt();
-			System.out.println(nbAvion);*/
-			//s.receive( packet ) ;
-			S = chaine.split("/");
-			nbAvion = Integer.parseInt(S[0]);
+                // Cette méthode permet de récupérer le datagramme envoyé par le client
+                // Elle bloque le thread jusqu'à ce que celui-ci ait reçu quelque chose.
+				s.receive(packet);
 
-                        Avion avion[]=new Avion[nbAvion];
-			for (i=0;i<nbAvion;i++){
-				avion[i]=new Avion();
-			}
+				String chaine = new String(packet.getData());
+                /*B = ByteBuffer.wrap(packet.getData());
+                nbAvion=B.getInt();
+                System.out.println(nbAvion);*/
+				// s.receive( packet ) ;
+                S = chaine.split("/");
+                nbAvion = Integer.parseInt(S[0]);
 
-			n=0;
-
-                        for (i=0;i<nbAvion;i++){
-				avion[i].setId(S[1+n]);
-				avion[i].setX(Integer.parseInt(S[2+n]));
-				avion[i].setY(Integer.parseInt(S[3+n]));
-				avion[i].setAltitude(Integer.parseInt(S[4+n]));
-				avion[i].setCap(Integer.parseInt(S[5+n]));
-				avion[i].setVitesse(Integer.parseInt(S[6+n]));
-				n=n+6;
-		          /*B.get(rawCode, 0, 6);
-		          avion[i].setId(new String(rawCode));
-		          avion[i].setX(B.getInt());
-		          avion[i].setY(B.getInt());
-		          avion[i].setAltitude(B.getInt());
-		          avion[i].setCap(B.getInt());
-		          avion[i].setVitesse(B.getInt());*/
-                        }
-
-			System.out.print("\033[H\033[2J");
-
-			System.out.println("LE NOMBRE D'AVION EST DE " + nbAvion);
-
-                        for (i=0;i<nbAvion;i++){
-                            Affiche(avion[i]);
-                        }
-                        
-                        //On réinitialise la taille du datagramme, pour les futures réceptions
-                        packet.setLength(buffer.length);
-                    }
-                } catch (SocketException e) {
-                   e.printStackTrace();
-                } catch (IOException e) {
-                   //TODO Auto-generated catch block
-                   e.printStackTrace();
+                Avion avion[] = new Avion[nbAvion];
+                for (i = 0; i < nbAvion; i++) {
+                	avion[i] = new Avion();
                 }
-            }
-        //});
-    //}
-    public void Affiche(Avion avion){
-        System.out.println("*************************");
-        System.out.println("** Console d'affichage **");
-        System.out.println("*************************");
-        System.out.println("** ID de l'avion : " + avion.getId());
-        System.out.println("** Coordonnée :");
-        System.out.println("**\t-X : " + avion.getX());
-        System.out.println("**\t-Y : " + avion.getY());
-	System.out.println("**");
-        System.out.println("** Altitude : " + avion.getAltitude());
-        System.out.println("** Cap : " + avion.getCap());
-        System.out.println("** Vitesse : " + avion.getVitesse());
-    }
+
+                n = 0;
+
+                for (i = 0; i < nbAvion; i++) {
+                	avion[i].setId(S[1+n]);
+                	avion[i].setX(Integer.parseInt(S[2+n]));
+                	avion[i].setY(Integer.parseInt(S[3+n]));
+                	avion[i].setAltitude(Integer.parseInt(S[4+n]));
+                	avion[i].setCap(Integer.parseInt(S[5+n]));
+                	avion[i].setVitesse(Integer.parseInt(S[6+n]));
+                	n=n+6;
+					/*B.get(rawCode, 0, 6);
+					avion[i].setId(new String(rawCode));
+					avion[i].setX(B.getInt());
+					avion[i].setY(B.getInt());
+					avion[i].setAltitude(B.getInt());
+					avion[i].setCap(B.getInt());
+					avion[i].setVitesse(B.getInt());*/
+				}
+				/**
+				 * Commande pour clean l'écran
+				 */
+				System.out.print("\033[H\033[2J");
+
+				System.out.println("LE NOMBRE D'AVION EST DE " + nbAvion);
+
+				for (i=0;i<nbAvion;i++){
+					Affiche(avion[i]);
+				}
+
+                //On réinitialise la taille du datagramme, pour les futures réceptions
+		      	packet.setLength(buffer.length);
+		  	}
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+                   //TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void Affiche(Avion avion) {
+		System.out.println("*************************");
+		System.out.println("** Console d'affichage **");
+		System.out.println("*************************");
+		System.out.println("** ID de l'avion : " + avion.getId());
+		System.out.println("** Coordonnée :");
+		System.out.println("**\t-X : " + avion.getX());
+		System.out.println("**\t-Y : " + avion.getY());
+		System.out.println("**");
+		System.out.println("** Altitude : " + avion.getAltitude());
+		System.out.println("** Cap : " + avion.getCap());
+		System.out.println("** Vitesse : " + avion.getVitesse());
+	}
 }
